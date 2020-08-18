66ec0c75e1bea0471371015c8dd699c86a216aac
Milad Awad <millad.99.awad@gmail.com>
2020-08-18 17:57:44 +0300

    Added geolocator dep

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    try {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      print(position);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    getLocation();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String myMargin = '15';
+    double myMarginAsADouble;
+    try {
+      myMarginAsADouble = double.parse(myMargin);
+    } catch (e) {
+      print(e);
+    }
+
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.all(
+          myMarginAsADouble ?? 30.0,
+        ),
+        color: Colors.red,
+      ),
+    );
+  }
+}
